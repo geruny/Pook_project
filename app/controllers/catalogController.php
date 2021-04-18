d5717ba0e6d0d709cd610fed57b2c990a4885a9f
geruny <gerunuky@gmail.com>
2021-04-18 23:16:27 +0300

    add books in catalog

--- app/controllers/catalogController.php
+++ app/controllers/catalogController.php
@@ -7,16 +7,12 @@ class CatalogController extends Controller
         parent::__construct($prefix);
 
         $this->view->setTitle("Каталог");
-    }
 
-    public function index()
-    {
         if ($books = $this->model->getListBooks())
             $this->view->arResult["books"] = $books;
         else
             $this->view->arResult["books"] = array();
 
-        parent::index();
     }
     
 }
--- app/views/catalogController/index.php
+++ app/views/catalogController/index.php
@@ -2,33 +2,29 @@
 
 <h1>Каталог</h1>
 
-<?= Libs\App::includeComponent('catalog') ?>
+<!-- Libs\App::includeComponent('catalog')  -->
 
 <? if(count($this->arResult['books'])>0):?>
 <table class="table table-dark table-striped">
     <thead>
         <tr>
-            <th>ID</th>
             <th>Название</th>
-            <th>Дата</th>
-            <th>Описание</th>
-            <th>Жанр</th>
             <th>Автор</th>
-            <th>Активность</th>
+            <th>Жанр</th>
+            <th>Дата выхода</th>
+            <th>Описание</th>
             <th>Изображение</th>
         </tr>
     </thead>
     <tbody>
-        <? foreach($this->arResult['books'] as $product): ?>
+        <? foreach($this->arResult['books'] as $book): ?>
         <tr>
-            <td><?= $product["books_id"] ?></td>
-            <td><?= $product["title"] ?></td>
-            <td><?= $product["date"] ?></td>
-            <td><?= $product["description"] ?></td>
-            <td><?= $product["genres"] ?></td>
-            <td><?= $product["writers"] ?></td>
-            <td><?= $product["active"] ?></td>
-            <td><?= strlen($product["pic"]) > 0 ? "<img src='" . MAIN_PREFIX . "/upload/{$product["pic"]}' width='50px' />" : "" ?></td>
+            <td><?= $book["title"] ?></td>
+            <td><?= $book["writers_name"] ?></br><?= $book["middle_name"] ?></td>
+            <td><?= $book["genres_name"] ?></td>
+            <td><?= $book["date"] ?></td>
+            <td><?= $book["description"] ?></td>
+            <td><?= strlen($book["pic"]) > 0 ? "<img src='" . MAIN_PREFIX . "/upload/{$book["pic"]}' width='50px' />" : "" ?></td>
         </tr>
         <? endforeach;?>
     </tbody>
@@ -36,7 +32,7 @@
 </table>
 <? else:?>
 <div class="alert alert-danger error_danger" role="alert">
-    Нет категорий по вашему запросу
+    Ошибка вывода каталога или каталог пуст :(
 </div>
 <? endif ?>
 
