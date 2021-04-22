45877b3a7c2d5a22d1a444bfa1b71760ec9a23ce
geruny <gerunuky@gmail.com>
2021-04-22 16:10:57 +0300

    edits style & html

--- app/views/indexController/index.php
+++ app/views/indexController/index.php
@@ -1,11 +1,29 @@
 <? require_once DIR_PATH_APP.'/views/header.php'?>
 
 <div class="container" id="index">
-    <h1>Книжный архив POOK</h1>
-    <p>Наш девиз - минимализм!</p>
-    <p>Ничего лишнего, обсалютно ничего</p>
+    <div class="row">
+        <div class="col-5">
+            <div class="main">
+                <h1>Книжный архив POOK</h1>
+                <p class="p_margin">Наш девиз - минимализм!</p>
+                <p>Ничего лишнего, обсалютно ничего</p>
+                <a class="nav-link" aria-current="page" href="<?= MAIN_PREFIX ?>/catalog">Перейти в каталог</a>
+            </div>
+        </div>
+        <div class="col-7">
+            <div class="row">
+                <div class="serg">
+                    <img class="img" src="<?= TEMPLATE_PATH ?>/assets/ser.jpg" alt="Sergio">
+                    <p>При поддержке:</br></br>Сергея Ивановича Михеева</br>Риты Ершовой</p>
+                </div>
+            </div>
+        </div>
 
-    <a class="nav-link" aria-current="page" href="<?= MAIN_PREFIX ?>/catalog">Перейти в каталог</a>
+    </div>
+
+
+</div>
+
 </div>
 
 <? require_once DIR_PATH_APP.'/views/footer.php'?>
--- app/views/lkController/index.php
+++ app/views/lkController/index.php
@@ -1,10 +1,12 @@
 <? require_once DIR_PATH_APP.'/views/header.php'?>
 
-<h1>Личный кабинет <?= Libs\User::getLogin() ?></h1>
+<div class="container">
+    <h1>Личный кабинет <?= Libs\User::getLogin() ?></h1>
 
-<h2>Данные о аккаунте</h2>
-<h3>Логин: <?= Libs\User::getLogin() ?></h3>
-<h3>Имя: <?= Libs\User::getName() ?></h3>
-<h3>E-mail: <?= Libs\User::getEmail() ?></h3>
+    <h2>Данные о аккаунте</h2>
+    <h3>Логин: <?= Libs\User::getLogin() ?></h3>
+    <h3>Имя: <?= Libs\User::getName() ?></h3>
+    <h3>E-mail: <?= Libs\User::getEmail() ?></h3>
+</div>
 
 <? require_once DIR_PATH_APP.'/views/footer.php'?>
